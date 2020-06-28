aff995fa1d15da785d266b57a08585d7c578633a
ElijahCoding <cuiyeqing960904@gmail.com>
2020-06-29 00:58:55 +0300

    refactored

--- src/DiskMonitorServiceProvider.php
+++ src/DiskMonitorServiceProvider.php
@@ -11,37 +11,68 @@ class DiskMonitorServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__ . '/../config/disk-monitor.php' => config_path('disk-monitor.php'),
-            ], 'config');
+        $this->registerPublishables()
+             ->registerCommands()
+             ->registerRoutes()
+             ->registerViews();
+    }
 
-            $this->publishes([
-                __DIR__.'/../resources/views' => base_path('resources/views/vendor/disk-monitor'),
-            ], 'views');
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/disk-monitor.php', 'disk-monitor');
+    }
 
-            if (! class_exists('CreatePackageTable')) {
-                $this->publishes([
-                    __DIR__ . '/../database/migrations/create_disk_monitor_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_disk_monitor_table.php'),
-                ], 'migrations');
-            }
-
-            $this->commands([
-                RecordDiskMetricsCommand::class,
-            ]);
+    protected function registerPublishables(): self
+    {
+        if (!$this->app->runningInConsole()) {
+            return $this;
         }
 
+        $this->publishes([
+            __DIR__ . '/../config/disk-monitor.php' => config_path('disk-monitor.php'),
+        ], 'config');
+
+        $this->publishes([
+            __DIR__ . '/../resources/views' => base_path('resources/views/vendor/disk-monitor'),
+        ], 'views');
+
+        if (!class_exists('CreatePackageTable')) {
+            $this->publishes([
+                __DIR__ . '/../database/migrations/create_disk_monitor_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_disk_monitor_table.php'),
+            ], 'migrations');
+        }
+
+        return $this;
+    }
+
+    protected function registerCommands(): self
+    {
+        if (!$this->app->runningInConsole()) {
+            return $this;
+        }
+
+        $this->commands([
+            RecordDiskMetricsCommand::class,
+        ]);
+
+        return $this;
+    }
+
+    protected function registerRoutes(): self
+    {
         Route::macro('diskMonitor', function (string $prefix) {
             Route::prefix($prefix)->group(function () {
                 Route::get('/', DiskMetricsController::class);
             });
         });
 
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'disk-monitor');
+        return $this;
     }
 
-    public function register()
+    protected function registerViews(): self
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/disk-monitor.php', 'disk-monitor');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'disk-monitor');
+
+        return $this;
     }
 }
